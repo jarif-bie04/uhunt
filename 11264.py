f8dc51fb129b7df88d7b3a08d6eef960468f10d0
Jarif Ayman <jarif.2409001@bau.edu.bd>
2026-09-03 15:24:19 +0600

    Create 11264.py

---
+++ 11264.py
@@ -0,0 +1,12 @@
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    coin_value = list(map(int, input().split()))
+
+    coin_count = 2
+    sum = coin_value[0]
+    for i in range(1, n-1):
+        if sum + coin_value[i] < coin_value[i+1]:
+            coin_count += 1
+            sum += coin_value[i]
+    print(coin_count)
